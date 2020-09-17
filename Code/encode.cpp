f073bc866df1ceab20bc97e5fc667d457d475e8f
Matin Massoudi <matin@massoudi.net>
2020-09-16 22:16:26 -0700

    renamed main.cpp

--- Code/encode.cpp
+++ Code/encode.cpp
@@ -1,8 +1,6 @@
 //
 // Created by Matin Massoudi on 9/2/20.
 //
-
-
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,14 +9,150 @@
 
 using namespace std;
 
-class encode{
+/**
+ * Encode string using insertion sort algorithm.
+ * @param input
+ * @return
+ */
+static string insertionSortEncode(string inputString)
+{
+    //variable declarations used for algorithm.
+    string input = inputString;
+    string originalInput = inputString;
+    int n = input.length();
+    string stringArray[n];
 
-
-    static string insertionEncode(string input)
+    //Shift and store strings within StringArray.
+    stringArray[0] = input;
+    for(int i = 1; i < n; i++)
     {
+        char temp = input[0];
+        int j;
+        for(j = 0; j < n-1; j++)
+        {
+            input[j] = input[j+1];
+        }
+        input[j] = temp;
+
+        stringArray[i] = input;
+    }
+
+    //Assign pointers to each string element within stringArray.
+    string* stringPointers[n];
+    for(int i = 0; i < n; i++)
+    {
+        stringPointers[i] = &stringArray[i];
+    }
+
+    //Sort pointer array using insertion sort.
+    int j;
+    int i;
+    string* key;
+    for (i = 1; i < n; i++)
+    {
+        key = stringPointers[i];
+        j = i - 1;
+        //Find and swap pointers within array of pointers.
+        while (j >= 0 && (*stringPointers[j]).compare(*key) > 0)
+        {
+            stringPointers[j + 1] = stringPointers[j];
+            j = j - 1;
+        }
+        stringPointers[j + 1] = key;
+    }
+
+    //Find position of original string in pointerArray.
+    int originalIndex;
+    for(int i = 0; i < n; i++)
+    {
+        if((*stringPointers[i]).compare(originalInput) == 0)
+        {
+            originalIndex = i;
+            break;
+        }
+    }
+
+    //Create a string of only the last chars. Used for final output.
+    string lastChars = "";
+    for(int i = 0; i < n; i++)
+    {
+        lastChars += (*stringPointers[i])[n-1];
+    }
+
+    //Set every element in frequency to default value -1.
+    int freq[n];
+    for(int i = 0; i < n; i++)
+    {
+        freq[i] = -1;
+    }
+
+    //Tally last row of string column and store into array.
+    int incrementor;
+    for(int i = 0; i < n; i++)
+    {
+        //Find consecutive characters, and tally. Store tally result into an array, then move on
+        //To next char sequence.
+        char last = (*stringPointers[i])[n-1];
+        incrementor = 0;
+        for(int j = i+1; j < n; j++)
+        {
+            if(last == (*stringPointers[j])[n-1])
+            {
+                incrementor++;
+            } else
+            {
+                break;
+            }
+        }
+        i+=incrementor;
+
+        //Assign positions to incrementor.
+        freq[i] = incrementor+1;
 
     }
-};
+
+    //Concat stores result of decoded string.
+    //Concat results with return string returnVal.
+    string concat = "";
+    for(int i = 0; i < n; i++)
+    {
+        if (freq[i] != -1)
+        {
+            //Remove last space.
+            if ((freq[i] != -1) && (i == n-1))
+            {
+                concat += to_string(freq[i])+" "+lastChars[i];
+            }else {
+                concat += to_string(freq[i])+" "+lastChars[i]+" ";
+            }
+
+        }
+    }
+
+    //Concatenate the index of original string with the decoded output, then return.
+    return to_string(originalIndex)+"\n"+concat;
+}
+
+int main(int argc, char** argv) {
+
+    //Parsing parameters disabled for milestone 1.
+    //Code will accept "insertion/quick" arguments, however it does not impact as of right now.
+    //cout << "Encoding Type:" << argv[1] << endl;
+
+    //Display each line of encoded text.
+    string input = "";
+    while (!getline(cin, input).eof()){
+        if (input.length() != 0)
+        {
+            cout << insertionSortEncode(input) << endl;
+        }else
+        {
+            cout << endl;
+        }
+    }
+
+    return 0;
+}
 
 
 
