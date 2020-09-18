cc32ee98798df03e27ed956e0d3b73f1912b6635
Matin Massoudi <matin@massoudi.net>
2020-09-17 18:11:20 -0700

    Stage 1 of implementing QuickSort

--- Code/encode.cpp
+++ Code/encode.cpp
@@ -133,12 +133,120 @@ static string insertionSortEncode(string inputString)
     return to_string(originalIndex)+"\n"+concat;
 }
 
+/**
+ * Encode string using quicksort algorithm.
+ * @param input
+ * @return
+ */
+static string quickSortEncode(string inputString)
+{
+    //variable declarations used for algorithm.
+    string input = inputString;
+    string originalInput = inputString;
+    int n = input.length();
+    string stringArray[n];
+
+    //Shift and store strings within StringArray.
+    stringArray[0] = input;
+    for(int i = 1; i < n; i++)
+    {
+        char temp = input[0];
+        int j;
+        for(j = 0; j < n-1; j++)
+        {
+            input[j] = input[j+1];
+        }
+        input[j] = temp;
+
+        stringArray[i] = input;
+    }
+
+    //Assign pointers to each string element within stringArray.
+    string* stringPointers[n];
+    for(int i = 0; i < n; i++)
+    {
+        stringPointers[i] = &stringArray[i];
+    }
+
+    //Sort pointer array using quicksort.
+
+
+    //Find position of original string in pointerArray.
+    int originalIndex;
+    for(int i = 0; i < n; i++)
+    {
+        if((*stringPointers[i]).compare(originalInput) == 0)
+        {
+            originalIndex = i;
+            break;
+        }
+    }
+
+    //Create a string of only the last chars. Used for final output.
+    string lastChars = "";
+    for(int i = 0; i < n; i++)
+    {
+        lastChars += (*stringPointers[i])[n-1];
+    }
+
+    //Set every element in frequency to default value -1.
+    int freq[n];
+    for(int i = 0; i < n; i++)
+    {
+        freq[i] = -1;
+    }
+
+    //Tally last row of string column and store into array.
+    int incrementor;
+    for(int i = 0; i < n; i++)
+    {
+        //Find consecutive characters, and tally. Store tally result into an array, then move on
+        //To next char sequence.
+        char last = (*stringPointers[i])[n-1];
+        incrementor = 0;
+        for(int j = i+1; j < n; j++)
+        {
+            if(last == (*stringPointers[j])[n-1])
+            {
+                incrementor++;
+            } else
+            {
+                break;
+            }
+        }
+        i+=incrementor;
+
+        //Assign positions to incrementor.
+        freq[i] = incrementor+1;
+
+    }
+
+    //Concat stores result of decoded string.
+    //Concat results with return string returnVal.
+    string concat = "";
+    for(int i = 0; i < n; i++)
+    {
+        if (freq[i] != -1)
+        {
+            //Remove last space.
+            if ((freq[i] != -1) && (i == n-1))
+            {
+                concat += to_string(freq[i])+" "+lastChars[i];
+            }else {
+                concat += to_string(freq[i])+" "+lastChars[i]+" ";
+            }
+
+        }
+    }
+
+    //Concatenate the index of original string with the decoded output, then return.
+    return to_string(originalIndex)+"\n"+concat;
+}
+
 int main(int argc, char** argv) {
 
-    //Parsing parameters disabled for milestone 1.
-    //Code will accept "insertion/quick" arguments, however it does not impact as of right now.
-    //cout << "Encoding Type:" << argv[1] << endl;
-
+    cout << "Encoding Type:" << argv[1] << endl;
+    
     //Display each line of encoded text.
     string input = "";
     while (!getline(cin, input).eof()){
